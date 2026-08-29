30189c38460c4ccbe36cc460fa2d72a3de235ff7
Odelia <amarachiasher7@gmail.com>
2026-08-29 19:59:39 -0400

    Add files via upload

---
+++ HelloScript.py
@@ -0,0 +1,13 @@
+########################################
+# Odelia Asher # oasher@usf.edu        #
+#--------------------------------------#
+# main.py: prints "Hello, World!" and  #
+#examines behavior of data types.      #
+########################################
+
+""" we want this script to PRINT the string Hello, World!
+"""
+print ("Hello, World!")
+
+a = "Hello, World!"
+print (a)
